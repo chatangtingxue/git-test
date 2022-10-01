139fed69dfed05d0161c275b9f2452ddf7ce51e1
hujidan123 <hujidan123@qq.com>
2022-10-01 23:04:08 +0800

    master 2 commit

--- src/main/java/com/zjh/lss.java
+++ src/main/java/com/zjh/lss.java
@@ -3,6 +3,8 @@ package com.zjh;
 public class lss {
     public static void main(String[] args) {
         System.out.println("1");
+        System.out.println("1");
+        System.out.println("3");
 
     }
 }
