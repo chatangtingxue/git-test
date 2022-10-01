f072cba9084f01171c0abcc6741239781c25d7bd
hujidan123 <104011303+hujidan123@users.noreply.github.com>
2022-10-01 23:21:37 +0800

    pull commit

--- src/main/java/com/zjh/lss.java
+++ src/main/java/com/zjh/lss.java
@@ -5,8 +5,10 @@ public class lss {
         System.out.println("1");
         System.out.println("1");
         System.out.println("3");
-        System.out.println("2");
         System.out.println("3");
-
+        System.out.println("3");
+        System.out.println("3");
+        System.out.println("3");
+        System.out.println("3");
     }
 }
